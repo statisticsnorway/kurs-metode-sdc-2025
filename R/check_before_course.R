0787a782abda9c776be48d77d4d92ff08241ac63
Vidar Norstein Klungre <74055337+vidarkl@users.noreply.github.com>
2025-11-10 16:09:37 +0100

    Check before course

---
+++ R/check_before_course.R
@@ -0,0 +1,13 @@
+# Run the following code to make sure that sdcMicro is installed and works as intended.
+# This requires a working installation of R (2.10 or higher) and sdcMicro.
+
+# Uncomment this to install sdcMicro from CRAN:
+install.packages("sdcMicro")
+
+library(sdcMicro) 
+data(francdat)
+f <- freqCalc(francdat, keyVars=c(2, 4:6), w = 8)
+f$fk
+
+# Output should be:
+# [1] 2 2 2 1 1 1 1 2
